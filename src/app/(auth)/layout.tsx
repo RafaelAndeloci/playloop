1eba8bc3ebc3dcdfd28ea5b93b24009e5d6eac18
Rafael Andeloci <rafaelandeloci.dev@gmail.com>
2025-03-05 20:00:42 -0300

    feat(auth): adding user authentication with clerk 🔐

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      {children}
+    </div>
+  );
+}
