f0282fdcb2464dc5bec4c4706690fe204c494a2e
Rafael Andeloci <rafaelandeloci.dev@gmail.com>
2025-04-16 15:50:17 -0300

    feat: adding infinite scroll to the studio

--- src/modules/studio/ui/view/studio-view.tsx
+++ src/modules/studio/ui/view/studio-view.tsx
@@ -3,7 +3,13 @@ import { VideosSection } from "../sections/videos-section";
 interface StudioViewProps {}
 export function StudioView({}: StudioViewProps) {
   return (
-    <div>
+    <div className="flex flex-col gap-y-6 pt-2.5">
+      <div className="px-4">
+        <h1 className="text-2xl font-bold">Channel content</h1>
+        <p className="text-xs text-muted-foreground">
+          Mange your channel content and videos
+        </p>
+      </div>
       <VideosSection />
     </div>
   );
